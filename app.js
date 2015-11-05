836ad9815335b448de9ee97050136166da784338
unknown <periniraffaele@gmail.com>
2015-11-05 10:26:42 +0100

    provetta 1 perini raffaele

--- app.js
+++ app.js
@@ -12,9 +12,34 @@
 var tmpl = ' <li id="ID">' +
            '  <h3>SENTENCE</h3>' +
            ' </li> ';
+var domanda = 0;
+var errori = 0;
+
+
+function check(frase){
+    if(domanda == data.length - 1){
+        $("#tot-good").text(data.length - errori -1);
+        $("#tot").text(data.length);
+        $(".btn-opt-continue").hide();
+        $(".final").show();
+    }
+    else if(frase != data[domanda].phrase_de){
+        errori++;
+    }
+    domanda++;
+}
 
 $(document).ready(function(){
-
+    
+    $(".sentences").text(data[0].phrase_en);
+    
+    $(".btn-opt-continue").click(function(){
+        var risposta = document.getElementById('form-ctrl').value;
+        check(risposta);        
+        if(domanda < data.length){
+            $(".sentences").text(data[domanda].phrase_en);
+        }
+    });
   
 });
 
